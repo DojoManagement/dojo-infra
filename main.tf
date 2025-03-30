517fa0cc4650c91561b9b2470cbc81dce3ec3cf2
Henrique Westphal <henriquewalves@gmail.com>
2025-03-30 14:27:46 -0300

    test

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
+variable "github_token" {
+  type    = string
+  default = ""
+}
+
 module "infrastructure" {
-  source = "github.com/DojoManagement/dojo-ft-modules.git//dojo-infra?ref=dojo-infra-0.0.1"
+  source = "git::https://${var.github_token}@github.com/DojoManagement/dojo-ft-modules.git//dojo-infra?ref=dojo-infra-0.0.1"
 
   project_name  = "dojo-management"
   project_desc  = "Dojo Management"
