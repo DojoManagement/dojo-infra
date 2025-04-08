ff52938eab2053ef7500d4961ee202bb1ef21575
Henrique Westphal <henriquewalves@gmail.com>
2025-04-07 23:07:37 -0300

    test

--- main.tf
+++ main.tf
@@ -1,5 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket   = "dojo-management-tfstate"
+    key      = "dojo-infra/dojo-management-tfstate"
+    region   = "sa-east-1"
+    encrypt  = true
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+  }
+}
+
+
 module "infrastructure" {
-  source = "git::https://github.com/DojoManagement/dojo-tf-modules.git//dojo-infra?ref=dojo-infra-0.0.2"
+  source = "git::https://github.com/DojoManagement/dojo-tf-modules.git//dojo-infra?ref=dojo-infra-0.0.1"
 
   project_name  = "dojo-management"
   project_desc  = "Dojo Management"
